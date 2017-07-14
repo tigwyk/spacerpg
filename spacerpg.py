aa08be3ca7abe2980d4a132e004cee69856ec550
Lee Ingram <lee@soylentcola.com>
2017-07-14 23:52:46 +0000

    Removed complexity

--- spacerpg.py
+++ spacerpg.py
@@ -6,57 +6,16 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 
-app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'spacerpg.db'),
-    USERNAME='admin',
-    PASSWORD='default'
-    ))
-
-if __name__ == '__main__':
-    app.run()
-
-def connect_db():
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
-
-def init_db():
-    db = get_db()
-    with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
-
-@app.cli.command('initdb')
-def initdb_command():
-    init_db()
-    print('Initialized the database.')
-
-def get_db():
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
-
-@app.teardown_appcontext
-def close_db(error):
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
-
 
 @app.route('/')
 def show_entries():
-    db = get_db()
-    cur = db.execute('select title, text from entries order by id desc')
-    entries = cur.fetchall()
+    entries = ""
     return render_template('show_entries.html', entries=entries)
 
 @app.route('/add', methods=['POST'])
 def add_entry():
     if not session.get('logged_in'):
         abort(401)
-    db = get_db()
-    db.execute('insert into entries (title, text) values (?, ?)',
-            [request.form['title'], request.form['text']])
-    db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
 
--- spacerpg/__init__.py
+++
@@ -1,1 +0,0 @@
-from .spacerpg import app

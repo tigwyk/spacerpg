a646ce7faadbf3637a4d1ccfe302e7a382e5c4cc
Lee Ingram <lee@soylentcola.com>
2017-07-15 01:14:17 +0000

    Cleaning up my mistakes

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web gunicorn app:spacerpg
+web gunicorn app:app
--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 
 app = Flask(__name__)

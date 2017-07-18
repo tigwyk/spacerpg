f65fe6113e72bb86239c05450def2e10f76b990f
Lee Ingram <lee@soylentcola.com>
2017-07-18 22:24:58 +0000

    Lots of changes

--- login.py
+++ login.py
@@ -1,6 +1,7 @@
 import flask_login
 
 from app import app
+from models import User
 
 login_manager = flask_login.LoginManager()
 
@@ -8,7 +9,7 @@ login_manager.init_app(app)
 
 @login_manager.user_loader
 def user_loader(user_id):
-    return User.get(user_id)
+    return User.query.get(user_id)
 
 @login_manager.unauthorized_handler
 def unauthorized_handler():

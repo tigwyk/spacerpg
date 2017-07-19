b41065b59e4ae2027023d0db70a432c4baa5037b
Lee Ingram <lee@soylentcola.com>
2017-07-19 05:53:48 +0000

    Fixed some glaring issues

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
 
-from models import News,User
+from models import News,User,Character,Weapon,Item,Armor
 from login import login_manager
 from forms import RegistrationForm,LoginForm,CharacterCreationForm
 
--- manage.py
+++ manage.py
@@ -3,8 +3,7 @@ import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-from app import app
-from models import db
+from app import app,db
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 

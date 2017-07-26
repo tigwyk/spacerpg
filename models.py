d558bcaa3a831f116822c5f4a30ed8b889f20adc
Lee Ingram <lee@soylentcola.com>
2017-07-26 05:16:54 +0000

    This isn't ready for showtime yet

--- models.py
+++ models.py
@@ -74,7 +74,7 @@ class Room(db.Model):
     players = db.relationship('Character', backref='location',lazy='dynamic')
     description = db.Column(db.String(256))
     exit_id = db.Column(db.Integer, db.ForeignKey('room.id'), index=True)
-    exits = db.relation('Room', remote_side=[id],backref='exits')
+    exits = db.relation('Room', remote_side=[id],backref='linked_rooms')
 
     def __init__(self, name):
         self.name = name

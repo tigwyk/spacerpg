80629f0f4818cc2ee41c75e2013a9790fbfd9003
Lee Ingram <lee@soylentcola.com>
2017-07-29 02:26:19 +0000

    Still breaking stuff

--- models.py
+++ models.py
@@ -226,8 +226,9 @@ class Living(db.Model):
             return False
 
         item_slot = item.slot
-        updated_body = self.body.update({item_slot : item.id})
-        self.body = updated_body
+        current_body_array = self.body
+        current_body_array.update({item_slot : item.id})
+        self.body = current_body_array
         db.session.add(self)
         db.session.commit()
         return True

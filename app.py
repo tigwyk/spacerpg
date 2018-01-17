170d9773bb6b350b9c7d6a27ba6b617f7983338d
Lee Ingram <lee@soylentcola.com>
2018-01-17 22:59:00 +0000

    Playing around with how JSON data is returned

--- app.py
+++ app.py
@@ -313,6 +313,12 @@ def api_room_lookup(room_id):
     room = Room.query.get(room_id)
     return jsonify(room.as_dict())
 
+@flask_login.login_required
+@api_bp.route("/room2/<int:room_id>")
+def api_room_lookup(room_id):
+    room = Room.query.get(room_id)
+    return jsonify(dict(room))
+
 #@app.route('/move/<int:destination_id>')
 @flask_login.login_required
 @api_bp.route('/move/<int:destination_id>')
